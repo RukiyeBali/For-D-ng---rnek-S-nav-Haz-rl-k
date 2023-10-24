b81c7dfa2b6a22e20a145fbea04922f522355287
Rukiye Balı <145977861+RukiyeBali@users.noreply.github.com>
2023-10-24 14:55:58 +0300

    Add files via upload

---
+++ dongu-ornekler/dongu-ornekler/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace dongu_ornekler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNotlar = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstKalanlar = new System.Windows.Forms.ListBox();
+            this.lstGecenler = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstNotlar
+            // 
+            this.lstNotlar.FormattingEnabled = true;
+            this.lstNotlar.Location = new System.Drawing.Point(163, 46);
+            this.lstNotlar.Name = "lstNotlar";
+            this.lstNotlar.Size = new System.Drawing.Size(191, 472);
+            this.lstNotlar.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Notları Oluştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstKalanlar
+            // 
+            this.lstKalanlar.FormattingEnabled = true;
+            this.lstKalanlar.Location = new System.Drawing.Point(473, 46);
+            this.lstKalanlar.Name = "lstKalanlar";
+            this.lstKalanlar.Size = new System.Drawing.Size(185, 472);
+            this.lstKalanlar.TabIndex = 2;
+            // 
+            // lstGecenler
+            // 
+            this.lstGecenler.FormattingEnabled = true;
+            this.lstGecenler.Location = new System.Drawing.Point(664, 46);
+            this.lstGecenler.Name = "lstGecenler";
+            this.lstGecenler.Size = new System.Drawing.Size(185, 472);
+            this.lstGecenler.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(375, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 62);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Geçen Kalan Dağıt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(522, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Kalanlar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(723, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Geçenler";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(888, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Geçen:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(888, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Kalan:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(893, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Teşekkür Belgesi: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(893, 239);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Teşekkür Belgesi: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(893, 290);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Düz Geçenler Sayısı:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(893, 334);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "En Yüksek Not: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(893, 370);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(80, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "En Düşük Not: ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(893, 414);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(78, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Sınıf Ortalama: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 645);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lstGecenler);
+            this.Controls.Add(this.lstKalanlar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstNotlar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNotlar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstKalanlar;
+        private System.Windows.Forms.ListBox lstGecenler;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ dongu-ornekler/dongu-ornekler/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dongu_ornekler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lstNotlar.Items.Clear();
+            Random rnd=new Random();
+
+            for (int i = 1; i <= 40; i++)
+            {
+               lstNotlar.Items.Add(rnd.Next(1, 101));
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lstKalanlar.Items.Clear();
+            lstGecenler.Items.Clear();
+            int not;
+            int gecenSayisi = 0;
+            int kalanSayisi = 0;
+            int tesekkurSayisi = 0;
+            int takdirSayisi = 0;
+            int duzGecen = 0;
+
+            int enYuksekPuan = 0;
+
+            int enDusukPuan = 100;
+
+            double sinifToplam = 0;
+            double sinifOrtalama=0;
+
+
+
+
+
+            for(int i = 0; i < lstNotlar.Items.Count; i++)
+            {
+                not = Convert.ToInt32(lstNotlar.Items[i]);
+
+                if (not >= 50)
+                {
+                    lstGecenler.Items.Add(not);
+                    gecenSayisi++;
+                }
+                else
+                {
+                    lstKalanlar.Items.Add(not);
+                    kalanSayisi++;
+                }
+                if(not>=70 && not < 85)
+                {
+                    tesekkurSayisi++;
+                }
+                if (not >= 85)
+                {
+                    takdirSayisi++;
+                }
+                if (not>=50&& not < 70)
+                {
+                    duzGecen++;
+                }
+
+
+                if (not > enYuksekPuan)
+                {
+                    enYuksekPuan = not;
+                }
+
+                if (not < enDusukPuan)
+                {
+                    enDusukPuan = not;
+                }
+
+
+
+                sinifToplam += not;
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+            label3.Text = "Geçen: " + gecenSayisi;
+            label4.Text="Kalan: " + kalanSayisi;
+            label5.Text = "Teşekkür Belgesi: " + tesekkurSayisi;
+            label6.Text = "Takdir Belgesi. " + takdirSayisi;
+            label7.Text = "Belge Almadan Geçen: " + duzGecen;
+            label8.Text = "En Yüksek Not: " + enYuksekPuan;
+            label9.Text = "En Düşük Not: " + enDusukPuan;
+            label10.Text = "Sınıf Ortalaması: " + sinifOrtalama;
+
+
+
+        }
+    }
+}
